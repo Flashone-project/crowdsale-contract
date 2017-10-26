317c2caa1f651a01a777d79eeda0f49944350057
Sardor Isakov <sisardor@gmail.com>
2017-10-25 18:16:02 -0700

    minor

--- truffle.js
+++ truffle.js
@@ -4,6 +4,13 @@ module.exports = {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    rinkeby: {
+      host: "localhost", // Connect to geth on the specified
+      port: 8545,
+      from: "0x2DAd2118Ec0D8105030482b5d0ECD09D6A3aa395", // default address to use for any transaction Truffle makes during migrations
+      network_id: 4,
+      gas: 4612388 // Gas limit used for deploys
     }
   }
 };
